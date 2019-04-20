0b1b0a2149da03be467a235b54d02d4d5617dd9b
astrophobos <dsungwyndolin@gmail.com>
2019-04-20 17:34:47 +0300

    1.05 Suit loaded -enlightment

--- main.cpp
+++ main.cpp
@@ -97,8 +97,7 @@ int main()
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
